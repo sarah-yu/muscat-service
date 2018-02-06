940db0524902bf33cd7d87d6d12e27a9ad723d3d
Sarah Yu <sarahyu0692@gmail.com>
2018-02-05 23:00:38 -0500

    set up express server and add sample test

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const parser = require('body-parser')
+
+const app = express()
+
+const petsController = require('./controllers/pets')
+
+app.use(parser.json())
+app.use(parser.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+	res.send('hello world')
+})
+
+app.use('/api/pets', petsController)
+
+app.listen(3000, () => {
+	console.log('express server up and running')
+})
---
+++ test/pets.js
@@ -0,0 +1,13 @@
+const should = require('chai').should()
+const expect = require('chai').expect
+const supertest = require('supertest')
+const service = supertest('http://localhost:3000')
+
+describe('GET /api/pets', () => {
+	it('should return a 200 response', done => {
+		service
+			.get('/api/pets')
+			.set('Accept', 'application/json')
+			.expect(200, done)
+	})
+})
