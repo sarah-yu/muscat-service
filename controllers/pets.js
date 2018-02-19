b3682dd4f2060188be49ebdc4bb0fb5d9c5e4888
Sarah Yu <sarahyu0692@gmail.com>
2018-02-19 12:00:33 -0500

    encrypt heroku api key

--- controllers/pets.js
+++ controllers/pets.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const axios = require('axios')
 
 router.get('/', (req, res) => {
-	res.send('getting all pets')
+	axios
+		.get(
+			'http://api.petfinder.com/pet.find?format=json&key=b5e210b2ec6323ecd5b96afa87eeb81b&location=20878'
+		)
+		.then(pets => {
+			res.json(pets.data['petfinder']['pets']['pet'])
+		})
+		.catch(err => console.log(err))
 })
 
 module.exports = router
--- test/pets.js
+++ test/pets.js
@@ -2,7 +2,8 @@ const should = require('chai').should()
 const expect = require('chai').expect
 const supertest = require('supertest')
 
-const service = supertest('http://muscat-service.herokuapp.com')
+// const service = supertest('http://muscat-service.herokuapp.com')
+const service = supertest('http://localhost:3000')
 
 describe('GET /api/pets', () => {
 	it('should return a 200 response', done => {
